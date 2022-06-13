9827a2f48b661edb73f1aa1a325753a99a8a3b42
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2022-06-14 00:03:36 +0530

    Updates

---
+++ src/compnonents/QuizResult.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const QuizResult = ({result,retry}) => {
+  return (
+    <div className='result-screen'>
+        <h2>Result : {result.precentage}%</h2>
+        <p>Selected {result.correct} correct options out of {result.total} questions.</p>
+        <button onClick={retry}>Retry</button>
+
+    </div>
+  )
+}
+
+export default QuizResult
--- src/compnonents/Quizscreen.jsx
+++ src/compnonents/Quizscreen.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import QuizResult from './QuizResult';
+import Question from './Question';
+import QuestionList from '../data/questions.json';
 
-const Quizscreen = () => {
-  return 'quiz-screen';
+const QuizScreen = ({retry }) => {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [markedAnswers, setMarkedAnswers] = useState(new Array(QuestionList.length));
+    const isQuesionEnd = currentQuestionIndex === QuestionList.length;
+
+    function calculateResult  () {
+        let correct = 0;
+        QuestionList.forEach((question,index)=>{
+            if(question.correctOptionIndex == markedAnswers[index]){
+                correct++;
+            }
+        });
+        return {
+            total : QuestionList.length,
+            correct : correct,
+            precentage : Math.trunc((correct / QuestionList.length) * 100)
+        }
+    }
+
+  return (
+    <div classNmae="quiz-screen" >
+        {
+            isQuesionEnd ? (<QuizResult 
+                result={calculateResult()}
+                retry={retry}
+
+            />) : 
+            (<Question 
+            question ={QuestionList[currentQuestionIndex]}
+            totalQuestions = {QuestionList.length}
+            currentQuestion = {currentQuestionIndex + 1}
+            setAnswer={(index)=>{
+                setMarkedAnswers((arr)=>{
+                    let newArr = [...arr];
+                    newArr[currentQuestionIndex] = index;
+                    return newArr;
+                });
+                setCurrentQuestionIndex(currentQuestionIndex + 1);
+            }}
+            />)
+        }
+    </div>
+  )
 }
 
-export default Quizscreen;
+export default QuizScreen;
